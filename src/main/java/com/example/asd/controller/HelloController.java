c0ec08ac9c0539026ef40d51cc708b67bbb23bf5
xialiang <xialiang@intest.cn>
2020-06-07 01:15:30 +0800

    第一次

--- src/main/java/com/example/asd/controller/HelloController.java
+++ src/main/java/com/example/asd/controller/HelloController.java
@@ -1,23 +1,15 @@
 package com.example.asd.controller;
 
-import com.example.asd.service.PersonServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
-    @Autowired
-    PersonServiceImpl personService;
+
 
     @RequestMapping(value = "/index/add", method = RequestMethod.POST)
-    public boolean add(@RequestBody Person json){
-        boolean user = personService.addPerson(json);
-        return user;
+    public String hello(){
+        return "user撒打算打算打算";
     }
 
-    @RequestMapping(value = "/index/delete", method = RequestMethod.POST)
-    public boolean delete(@RequestParam String name){
-        boolean user = personService.deletePerson(name);
-        return user;
-    }
 }
