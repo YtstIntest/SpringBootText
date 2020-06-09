0104860a1b5eca487061011bb8c75c2bad35846b
xialiang <xialiang@intest.cn>
2020-06-09 08:55:11 +0800

    00000000

--- src/main/java/com/example/asd/controller/HelloController.java
+++ src/main/java/com/example/asd/controller/HelloController.java
@@ -14,7 +14,7 @@ public class HelloController {
 
     @RequestMapping(value = "/index/ddd", method = RequestMethod.POST)
     public String kis(){
-        return "zhongyugaohaole";
+        return "2222222222";
     }
 
 }
