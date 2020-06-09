96a6156e5d634f039dd2cd8f0a32825c36ee88c2
xialiang <xialiang@intest.cn>
2020-06-09 13:35:52 +0800

    数据选项查询接口

--- src/main/java/com/example/asd/controller/HelloController.java
+++ src/main/java/com/example/asd/controller/HelloController.java
@@ -1,20 +1,62 @@
 package com.example.asd.controller;
 
+import com.example.asd.entity.SelectorBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class HelloController {
 
+    @RequestMapping(value = "/fota/zt/selector", method = RequestMethod.POST)
+    public SelectorBean selector(){
+        SelectorBean bean=new SelectorBean();
+        List<SelectorBean.DataBean> listD= new ArrayList<>();
+        SelectorBean.DataBean dataBean =new SelectorBean.DataBean();
+        SelectorBean.DataBean dataBean2 =new SelectorBean.DataBean();
+        List<SelectorBean.DataBean.RowListBean> listR= new ArrayList<>();
+        SelectorBean.DataBean.RowListBean rowListBean = new SelectorBean.DataBean.RowListBean();
+        List<SelectorBean.DataBean.RowListBean.ListOfFilterBean> listL= new ArrayList<>();
+        SelectorBean.DataBean.RowListBean.ListOfFilterBean listOfFilterBean=new SelectorBean.DataBean.RowListBean.ListOfFilterBean();
+        SelectorBean.DataBean.RowListBean.ListOfFilterBean listOfFilterBean2=new SelectorBean.DataBean.RowListBean.ListOfFilterBean();
 
-    @RequestMapping(value = "/index/add", method = RequestMethod.POST)
-    public String hello(){
-        return "user撒打算打算打算";
-    }
+        dataBean.setId(111);
+        dataBean.setChecked(false);
 
-    @RequestMapping(value = "/index/ddd", method = RequestMethod.POST)
-    public String kis(){
-        return "111112222233333";
+        rowListBean.setRowId(1113);
+        rowListBean.setTitle("地址");
+        rowListBean.setRowOrder("1");
+        rowListBean.setSortOrder(null);
+        rowListBean.setChecked(true);
+        rowListBean.setValue("'Joe'");
+        rowListBean.setWidth("300px");
+
+        listOfFilterBean.setText("'Joe'");
+        listOfFilterBean.setValue("'Joe'");
+        listOfFilterBean.setChecked(false);
+
+        listOfFilterBean2.setText("'Joe'");
+        listOfFilterBean2.setValue("'Joe'");
+        listOfFilterBean2.setChecked(false);
+
+        listL.add(listOfFilterBean);
+        listL.add(listOfFilterBean2);
+
+        listR.add(rowListBean);
+
+        listD.add(dataBean);
+
+
+        dataBean2=dataBean;
+        listD.add(dataBean2);
+
+        rowListBean.setListOfFilter(listL);
+        dataBean.setRowList(listR);
+        bean.setData(listD);
+
+        return bean;
     }
 
 }
---
+++ src/main/java/com/example/asd/entity/SelectorBean.java
@@ -0,0 +1,175 @@
+package com.example.asd.entity;
+
+import java.util.List;
+
+public class SelectorBean {
+
+    private List<DataBean> data;
+
+    public List<DataBean> getData() {
+        return data;
+    }
+
+    public void setData(List<DataBean> data) {
+        this.data = data;
+    }
+
+    public static class DataBean {
+        /**
+         * rowList : [{"rowId":1113,"title":"地址","rowOrder":"","sortOrder":"","listOfFilter":[{"text":"","value":"","checked":""}],"checked":true,"value":"","width":"300px"}]
+         * id : 222
+         * checked : false
+         */
+
+        private int id;
+        private boolean checked;
+        private List<RowListBean> rowList;
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public boolean isChecked() {
+            return checked;
+        }
+
+        public void setChecked(boolean checked) {
+            this.checked = checked;
+        }
+
+        public List<RowListBean> getRowList() {
+            return rowList;
+        }
+
+        public void setRowList(List<RowListBean> rowList) {
+            this.rowList = rowList;
+        }
+
+        public static class RowListBean {
+            /**
+             * rowId : 1113
+             * title : 地址
+             * rowOrder :
+             * sortOrder :
+             * listOfFilter : [{"text":"","value":"","checked":""}]
+             * checked : true
+             * value :
+             * width : 300px
+             */
+
+            private int rowId;
+            private String title;
+            private String rowOrder;
+            private String sortOrder;
+            private boolean checked;
+            private String value;
+            private String width;
+            private List<ListOfFilterBean> listOfFilter;
+
+            public int getRowId() {
+                return rowId;
+            }
+
+            public void setRowId(int rowId) {
+                this.rowId = rowId;
+            }
+
+            public String getTitle() {
+                return title;
+            }
+
+            public void setTitle(String title) {
+                this.title = title;
+            }
+
+            public String getRowOrder() {
+                return rowOrder;
+            }
+
+            public void setRowOrder(String rowOrder) {
+                this.rowOrder = rowOrder;
+            }
+
+            public String getSortOrder() {
+                return sortOrder;
+            }
+
+            public void setSortOrder(String sortOrder) {
+                this.sortOrder = sortOrder;
+            }
+
+            public boolean isChecked() {
+                return checked;
+            }
+
+            public void setChecked(boolean checked) {
+                this.checked = checked;
+            }
+
+            public String getValue() {
+                return value;
+            }
+
+            public void setValue(String value) {
+                this.value = value;
+            }
+
+            public String getWidth() {
+                return width;
+            }
+
+            public void setWidth(String width) {
+                this.width = width;
+            }
+
+            public List<ListOfFilterBean> getListOfFilter() {
+                return listOfFilter;
+            }
+
+            public void setListOfFilter(List<ListOfFilterBean> listOfFilter) {
+                this.listOfFilter = listOfFilter;
+            }
+
+            public static class ListOfFilterBean {
+                /**
+                 * text :
+                 * value :
+                 * checked :
+                 */
+
+                private String text;
+                private String value;
+                private boolean checked;
+
+                public String getText() {
+                    return text;
+                }
+
+                public void setText(String text) {
+                    this.text = text;
+                }
+
+                public String getValue() {
+                    return value;
+                }
+
+                public void setValue(String value) {
+                    this.value = value;
+                }
+
+                public boolean getChecked() {
+                    return checked;
+                }
+
+                public void setChecked(boolean checked) {
+                    this.checked = checked;
+                }
+            }
+        }
+    }
+
+}
